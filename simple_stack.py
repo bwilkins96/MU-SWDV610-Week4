792a0c2fb62a94668e9549e632583f44a3be99ff
bwilkins96 <ben.jr.speed@gmail.com>
2023-04-05 21:12:18 -0400

    simple stack

---
+++ simple_stack.py
@@ -0,0 +1,38 @@
+# SWDV 610: Data Structures and Algorithms
+# Simple stack implementation using a list
+
+class Stack:
+    """A list-based implementation of a stack that accepts only unique values"""
+
+    def __init__(self):
+        self._stack = []
+
+    def push(self, data):
+        if data not in self._stack:
+            self._stack.append(data)
+            return True
+        
+        return False
+    
+    def pop(self):
+        if len(self._stack) <= 0:
+            return('Stack empty!')
+        
+        return self._stack.pop()
+    
+    def size(self):
+        return len(self._stack)
+    
+if __name__ == '__main__':
+    s = Stack()
+    print(s.push(5))
+    print(s.push(9))
+    print(s.push(6))
+    print(s.push(5))
+
+    print(s.size())
+    print(s.pop())
+    print(s.pop())
+    print(s.pop())
+    print(s.size())
+    print(s.pop())
